a961e8f98d9dcc7d9bcdeba4265db990e4404352
C1122C <763443070@>
2017-12-04 21:29:04 +0800

    <dev>:<add method in model>

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dora
+ * Date: 2016/11/3
+ * Time: 22:07
+ */
+//Database
+define('DBHost', 'localhost');
+define('DBPort', '3306');
+define('DBName', 'forum');
+define('DBUser', 'root');
+define('DBPassword', '2a617');
+
+define('LanguagePath', './language/zh-cn/');
---
+++ data/PDO.class.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dora
+ * Date: 2016/11/3
+ * Time: 22:10
+ */
+class DB {
+
+    private $file_db;
+    private $sQuery;
+    public $queryCount = 0;
+
+    public function __construct() {
+        $this->file_db = new PDO('sqlite:./Graffiti.db');
+    }
+
+
+    private function Init($query, $parameters = "") {
+        try {
+            if($this->sQuery     = $this->file_db->prepare($this->BuildParams($query,$parameters))){
+                if (!empty($parameters)) {
+                    if (array_key_exists(0, $parameters)) {     //序号型
+                        $parametersType = true;
+                    } else {
+                        $parametersType = false;
+                    }
+                    foreach ($parameters as $column => $value) {
+//                        echo $parametersType ? $column+1 : ":" . $column." ". $parameters[$column].'<br>';
+                        $this->sQuery->bindParam($parametersType ? $column+1 : ":" . $column, $parameters[$column]);
+                    }
+                }
+                $this->sQuery->execute();
+                $this->queryCount++;
+            } else {
+                printf("Errormessage:\n");
+                foreach($this->file_db->errorInfo() as $line)
+                    printf("%s\n",$line);
+            }
+
+
+
+        } catch (PDOException $e) {
+            die();
+        }
+    }
+
+    private function BuildParams($query, $params = null)
+    {
+        if (!empty($params)) {
+            $rawStatement = explode(" ", $query);
+            foreach ($rawStatement as $value) {
+                if (strtolower($value) == 'in') {
+                    return str_replace("(?)", "(" . implode(",", array_fill(0, count($params), "?")) . ")", $query);
+                }
+            }
+        }
+        return $query;
+    }
+
+    public function save($query, $params = null, $fetchmode = PDO::FETCH_ASSOC) {
+        $query        = trim($query);
+        $this->Init($query, $params);
+        return $this->file_db->lastInsertId();
+    }
+
+    public function query($query, $params = null, $fetchmode = PDO::FETCH_ASSOC) {
+        $query        = trim($query);
+        $rawStatement = explode(" ", $query);
+        $this->Init($query, $params);
+        $statement = strtolower($rawStatement[0]);
+        if ($statement === 'select' || $statement === 'show') {
+            return $this->sQuery->fetchAll($fetchmode);
+        } elseif ($statement === 'insert' || $statement === 'update' || $statement === 'delete') {
+            return $this->sQuery->rowCount();
+        } else {
+            return NULL;
+        }
+    }
+
+    public function column($query, $params = null)
+    {
+        $this->Init($query, $params);
+        $resultColumn = $this->sQuery->fetchAll(PDO::FETCH_COLUMN);
+        //$this->rowCount = $this->sQuery->rowCount();
+        //$this->columnCount = $this->sQuery->columnCount();
+        $this->sQuery->closeCursor();
+        return $resultColumn;
+    }
+
+    public function row($query, $params = null, $fetchmode = PDO::FETCH_ASSOC)
+    {
+        $this->Init($query, $params);
+        $resultRow = $this->sQuery->fetch($fetchmode);
+        $this->sQuery->closeCursor();
+        return $resultRow;
+    }
+
+    public function single($query, $params = null)
+    {
+        $this->Init($query, $params);
+        return $this->sQuery->fetchColumn();
+    }
+}
